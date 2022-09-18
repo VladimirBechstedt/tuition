e94b591635fd9732d292a16aa1147e3304110691
vladimir <wowa1998bwp@gmail.com>
2022-09-18 15:57:56 +0600

    version1

---
+++ task_6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 1)
+{
+    Console.Write("Нет");
+}
+else
+{
+    Console.Write("Да");
+}
