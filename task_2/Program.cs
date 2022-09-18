3b7d1cf84c229e80c4ee7037a7f0951243931139
vladimir <wowa1998bwp@gmail.com>
2022-09-18 14:54:59 +0600

    virsion1

---
+++ task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b)
+{
+    Console.WriteLine("Эти числа равны");
+}
+if (a > b)
+{
+    Console.WriteLine($"max = {a}");
+}
+else
+{
+    Console.WriteLine($"max = {b}");
+}
