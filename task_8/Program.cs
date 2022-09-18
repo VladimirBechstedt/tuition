ad4f466b6b72f0a9e6c1f88434ee9ff393b5f977
vladimir <wowa1998bwp@gmail.com>
2022-09-18 16:15:42 +0600

    version1

---
+++ task_8/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+int i = 2;
+
+if (num < 1)
+{
+    Console.WriteLine("Число должно быть положительным");
+}
+if (num > 1)
+{
+    while (i < num + 1)
+    {
+        Console.Write($"{i}, ");
+        i += 2;
+    }
+}
