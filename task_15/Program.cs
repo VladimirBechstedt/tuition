0680651ca64c77741e5b8b4a724c3c2d5182a4de
vladimir <wowa1998bwp@gmail.com>
2022-09-21 00:17:50 +0600

    version1

---
+++ task_15/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите день недели: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num == 6 | num == 7)
+{
+    Console.Write("Да");
+}
+else if (num > 0 & num < 6)
+{
+    Console.Write("Нет");
+}
