940b525af32d01cd1969be638b8cc67cb7ed6314
vladimir <wowa1998bwp@gmail.com>
2022-09-20 23:48:42 +0600

    version1

---
+++ task_10/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 100 | num > 999)
+{
+    bool i = true;
+
+    while (i)
+    {
+        Console.Clear();
+        Console.Write("Введите трехзначное число: ");
+
+        num = Convert.ToInt32(Console.ReadLine());
+
+        if (num > 99 & num < 1000)
+        {
+            i = false;
+        }
+        else
+        {
+            //Console.Write("Введите трехзначное число: ");
+        
+        }
+    }
+}
+
+int a = num % 100;
+int b = a / 10;
+
+Console.WriteLine(b);
