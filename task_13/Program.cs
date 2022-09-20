82a9ce049605ef4a39a496a17cd04cc376bdb7ae
vladimir <wowa1998bwp@gmail.com>
2022-09-21 00:10:07 +0600

    version1

---
+++ task_13/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else if (num > 999)
+{
+    while (num > 999)
+    {
+        num /= 10;
+    }
+    int a = num % 10;
+    Console.WriteLine(a);
+}
+else
+{
+    int a = num % 10;
+    Console.WriteLine(a);
+}
