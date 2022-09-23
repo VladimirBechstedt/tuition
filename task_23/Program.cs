e774ed761f986b407c35f510fe43c055204a98fc
vladimir <wowa1998bwp@gmail.com>
2022-09-23 22:57:40 +0600

    Version1

---
+++ task_23/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (i <= num)
+{
+    Console.Write($"{Math.Pow(i, 3)}, ");
+    i++;
+}
