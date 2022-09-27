cb9da1aaaa861606fc9762f812ef6ea57a97585a
vladimir <wowa1998bwp@gmail.com>
2022-09-27 21:18:58 +0600

    Version1

---
+++ task_25/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int num = a;
+
+for(int i = 1; i < b; i++)
+{
+    num = num * a;
+}
+Console.WriteLine(num);
