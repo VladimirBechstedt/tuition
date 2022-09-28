ca9719993dce15d85407f134ee34f1a97506d3b4
vladimir <wowa1998bwp@gmail.com>
2022-09-28 23:41:05 +0600

    Version1

---
+++ task_29/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+int[] massiv = new int[8];
+Random rnd = new Random();
+
+for(int i = 0; i < massiv.Length; i++)
+{
+    massiv[i] = rnd.Next(0, 100);
+    Console.Write(massiv[i] + ", ");
+}
