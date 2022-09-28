e03a0046eea16ba1c7c0590ca1cf0c2ff65fd172
vladimir <wowa1998bwp@gmail.com>
2022-09-28 23:28:47 +0600

    Version1

---
+++ task_27/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool a = true;
+int sum = 0;
+
+while (a)
+{
+    if(num / 10 == 0)
+    {
+        a = false;
+    }
+    sum = sum + (num % 10);
+    num = num / 10;
+
+}
+Console.WriteLine(sum);
