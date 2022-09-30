50909d61e34a4f38216bfcf37767a18187471598
vladimir <wowa1998bwp@gmail.com>
2022-09-30 23:21:26 +0600

    Version1

---
+++ task_36/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] RandomArr(int start, int end, int LenghtArr)
+{
+    int[] arr = new int[LenghtArr];
+    for(int i = 0; i < LenghtArr; i++)
+    {
+        arr[i] = new Random().Next(start, end + 1);
+    }
+    return arr;
+}
+
+int[] arr = RandomArr(-99, 99, 10);
+int sum = 0;
+
+for(int i = 1; i < arr.Length; i = i + 2)
+{
+    sum = sum + arr[i];
+}
+Console.WriteLine(sum);
