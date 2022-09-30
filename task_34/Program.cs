eb5667c67480957919f49d4a2e03c6ae6f7b6297
vladimir <wowa1998bwp@gmail.com>
2022-09-30 23:08:02 +0600

    Version1

---
+++ task_34/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+
+void PrintArr(int[] value)
+{
+    for (int i = 0; i < value.Length; i++)
+    {
+        Console.Write(value[i]+ " ");
+    }
+    Console.WriteLine();
+}
+
+int[] RandomArr(int start, int end, int LenghtArr)
+{
+    int[] arr = new int[LenghtArr];
+    for(int i = 0; i < LenghtArr; i++)
+    {
+        arr[i] = new Random().Next(start, end + 1);
+    }
+    return arr;
+}
+
+int[] arr = RandomArr(100, 999, 12);
+PrintArr(arr);
+
+int even = 0;
+int odd = 0;
+for(int i = 0; i < arr.Length; i++)
+{
+    if(arr[i] % 2 == 0) even++;
+    else odd++;
+}
+Console.WriteLine(even);
+Console.WriteLine(odd);
