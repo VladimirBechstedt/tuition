e3f916470af79fa33457eba684869a6c535a861a
vladimir <wowa1998bwp@gmail.com>
2022-10-05 23:22:02 +0600

    version1

---
+++ task_41/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] arr = new int[5];
+int number = 0;
+
+for(int i = 0; i < arr.Length; i++)
+{
+    Console.Write("Введите число: ");
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+    if(arr[i] > 0) number++;
+    Console.WriteLine();
+}
+
+Console.Write(number);
