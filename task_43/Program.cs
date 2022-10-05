d964837b25ed18c0c6a5c02f427515e4c18b59ec
vladimir <wowa1998bwp@gmail.com>
2022-10-05 23:48:04 +0600

    version1

---
+++ task_43/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите координаты:");
+
+double[] b = {Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine())};
+
+Console.Write("Введите координаты:");
+
+double[] k = {Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine())};
+
+double x = (b[0] - b[1]) / (k[1] - k[0]);
+double y = k[0] * x + b[0];
+
+Console.WriteLine();
+Console.Write($"({x}; {y})");
