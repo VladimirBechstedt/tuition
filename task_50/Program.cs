19f51b308722d5a063cb0007bae3984a5d274da9
vladimir <wowa1998bwp@gmail.com>
2022-10-09 23:09:06 +0600

    version1

---
+++ task_50/Program.cs
@@ -0,0 +1,29 @@
+﻿int[,] RandomArr(int start, int end)
+{
+    int[,] arr = new int[6, 7];
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(start, end + 1);
+        }
+    }
+    return arr;
+}
+
+int[,] arr = RandomArr(0, 99);
+
+Console.Write("Введите цифру: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 0;
+
+for(int i = 0; i < arr.GetLength(0); i++)
+{
+    for(int j = 0; j < arr.GetLength(1); j++)
+    {
+        if(arr[i, j] == a) b++;
+    }
+}
+
+if(b >= 1) Console.Write("Такое число есть");
+else Console.Write("Такого числа нет");
