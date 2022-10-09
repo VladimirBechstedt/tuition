742622f8d1f3a6c17f3c5f14f521b70a685f9b20
vladimir <wowa1998bwp@gmail.com>
2022-10-09 22:31:24 +0600

    version1

---
+++ task_47/Program.cs
@@ -0,0 +1,30 @@
+﻿double[,] RandomArr(int row, int column)
+{
+    double[,] arr = new double[row, column];
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < column; j++)
+        {
+            arr[i, j] = Math.Round(new Random().NextDouble() * new Random().Next(-10, 10), 2);
+        }
+    }
+    return arr;
+}
+
+Console.Write("Введите колличество строк в массиве: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите колличество столбцов в массиве: ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+double[,] arr = RandomArr(row, column);
+
+for(int i = 0; i < arr.GetLength(0); i++)
+{
+    for(int j = 0; j < arr.GetLength(1); j++)
+    {
+        Console.Write($"{arr[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+
