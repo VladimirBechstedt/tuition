2db38bead87d5aaf515d15910f1a01efa6435751
vladimir <wowa1998bwp@gmail.com>
2022-10-20 00:10:19 +0600

    version1

---
+++ task_66/Program.cs
@@ -0,0 +1,12 @@
+﻿int SumSeries(int n, int m)
+{
+    if (n == m) return m;
+    return n + SumSeries(n + 1, m);
+}
+
+Console.Write("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(SumSeries(n, m));
