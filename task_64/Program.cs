093ef29174b12c78e9ce2e868938e15e45fa6d2b
vladimir <wowa1998bwp@gmail.com>
2022-10-19 23:53:31 +0600

    version1

---
+++ task_64/Program.cs
@@ -0,0 +1,9 @@
+﻿string row(int a)
+{
+    if (a == 1) return "1";
+    return Convert.ToString(a) + ", " + Convert.ToString(row(a - 1));
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write(row(num));
